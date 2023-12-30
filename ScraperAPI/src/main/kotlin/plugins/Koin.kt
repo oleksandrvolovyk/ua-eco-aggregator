4927fd5484ab80c6e0fb5883e2a06c7c9d2e7b9a
Oleksandr <olieksandr.volovik@gmail.com>
2023-12-30 19:25:42 +0200

    Dockerfiles, compose.yaml

--- ScraperAPI/src/main/kotlin/plugins/Koin.kt
+++ ScraperAPI/src/main/kotlin/plugins/Koin.kt
@@ -6,6 +6,7 @@ import org.koin.core.context.GlobalContext
 import org.koin.dsl.module
 import org.koin.ktor.plugin.Koin
 import org.koin.logger.slf4jLogger
+import kotlin.system.exitProcess
 
 fun Application.configureKoin() {
     install(Koin) {
@@ -17,11 +18,27 @@ fun Application.configureKoin() {
 
 val appModule = module {
     single {
+        val host = System.getenv("POSTGRES_HOST")
+        val port = System.getenv("POSTGRES_PORT")
+        val dbName = System.getenv("POSTGRES_DB_NAME")
+        val username = System.getenv("POSTGRES_USER")
+        val password = System.getenv("POSTGRES_PASS")
+
+        if (host == null || port == null || dbName == null || username == null || password == null) {
+            println("Required environment variable(s) not found!")
+            println("POSTGRES_HOST = $host")
+            println("POSTGRES_PORT = $port")
+            println("POSTGRES_DB_NAME = $dbName")
+            println("POSTGRES_USER = $username")
+            println("POSTGRES_PASS = $password")
+            exitProcess(1)
+        }
+
         Database.connect(
-            url = "jdbc:postgresql://localhost:5432/scraper",
-            user = "postgres",
+            url = "jdbc:postgresql://${host}:${port}/${dbName}",
+            user = username,
             driver = "org.postgresql.Driver",
-            password = "password"
+            password = password
         )
     }
     single { ScraperService(get()) }
