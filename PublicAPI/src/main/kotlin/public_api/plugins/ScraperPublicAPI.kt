4b5a47cfee09068b6341a5e53ee44b91b79465f1
Oleksandr <olieksandr.volovik@gmail.com>
2024-01-21 00:19:35 +0200

    PublicAPI: Scrapers

--- PublicAPI/src/main/kotlin/public_api/plugins/ScraperPublicAPI.kt
+++ PublicAPI/src/main/kotlin/public_api/plugins/ScraperPublicAPI.kt
@@ -1,9 +1,19 @@
 package public_api.plugins
 
 import ScraperService
+import io.ktor.http.*
 import io.ktor.server.application.*
+import io.ktor.server.response.*
 import io.ktor.server.routing.*
+import kotlinx.serialization.Serializable
+import model.Scraper
 import org.koin.ktor.ext.inject
+
+// Scraper without ApiKey
+@Serializable
+data class PublicScraper(val id: Int, val name: String)
+
+fun Scraper.toPublicScraper(): PublicScraper = PublicScraper(id = this.id, name = this.name)
 
 fun Application.configureScraperPublicAPI() {
     val scraperService by inject<ScraperService>()
@@ -11,7 +21,22 @@ fun Application.configureScraperPublicAPI() {
     routing {
         route("/api") {
             route("/scrapers") {
-                TODO()
+                // Get all scrapers
+                get {
+                    val scrapers = scraperService.readAll().map { it.toPublicScraper() }
+                    call.respond(HttpStatusCode.OK, scrapers)
+                }
+
+                // Read scraper by ID
+                get("/{id}") {
+                    val id = call.parameters["id"]?.toInt() ?: throw IllegalArgumentException("Invalid ID")
+                    val scraper = scraperService.read(id)?.toPublicScraper()
+                    if (scraper != null) {
+                        call.respond(HttpStatusCode.OK, scraper)
+                    } else {
+                        call.respond(HttpStatusCode.NotFound)
+                    }
+                }
             }
         }
     }
