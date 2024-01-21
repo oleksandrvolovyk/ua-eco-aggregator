467c93780e744988b958f1fc5702ae5cd40f7352
Oleksandr <olieksandr.volovik@gmail.com>
2024-01-21 21:43:53 +0200

    scrapers: Increase timeouts even more

--- LunMistoScraper/src/main/kotlin/Main.kt
+++ LunMistoScraper/src/main/kotlin/Main.kt
@@ -24,9 +24,9 @@ fun main() = runBlocking {
             gson()
         }
         install(HttpTimeout) {
-            requestTimeoutMillis = 60_000
-            connectTimeoutMillis = 60_000
-            socketTimeoutMillis = 60_000
+            requestTimeoutMillis = 600_000
+            connectTimeoutMillis = 600_000
+            socketTimeoutMillis = 600_000
         }
     }
 
--- MeteoGovUaScraper/src/main/kotlin/Main.kt
+++ MeteoGovUaScraper/src/main/kotlin/Main.kt
@@ -28,9 +28,9 @@ fun main(): Unit = runBlocking {
             gson()
         }
         install(HttpTimeout) {
-            requestTimeoutMillis = 60_000
-            connectTimeoutMillis = 60_000
-            socketTimeoutMillis = 60_000
+            requestTimeoutMillis = 600_000
+            connectTimeoutMillis = 600_000
+            socketTimeoutMillis = 600_000
         }
     }
 
