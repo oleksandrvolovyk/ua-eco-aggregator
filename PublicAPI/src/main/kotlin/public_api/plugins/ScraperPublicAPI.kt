99c6d23b27ca454e4c0168dc68f7673f7f20535c
Oleksandr <olieksandr.volovik@gmail.com>
2024-01-30 21:14:09 +0200

    public-api: Add information about the total number of records submitted by Scraper

--- PublicAPI/src/main/kotlin/public_api/plugins/ScraperPublicAPI.kt
+++ PublicAPI/src/main/kotlin/public_api/plugins/ScraperPublicAPI.kt
@@ -1,5 +1,7 @@
 package public_api.plugins
 
+import AirQualityService
+import RadiationService
 import ScraperService
 import io.ktor.http.*
 import io.ktor.server.application.*
@@ -9,30 +11,58 @@ import kotlinx.serialization.Serializable
 import model.Scraper
 import org.koin.ktor.ext.inject
 
-// Scraper without ApiKey
 @Serializable
-data class PublicScraper(val id: Int, val name: String)
+data class PublicScraper(
+    val id: Int,
+    val name: String,
+    val totalSubmittedAirQualityRecords: Long,
+    val totalSubmittedRadiationRecords: Long
+)
 
-fun Scraper.toPublicScraper(): PublicScraper = PublicScraper(id = this.id, name = this.name)
+fun Scraper.toPublicScraper(
+    totalSubmittedAirQualityRecords: Long,
+    totalSubmittedRadiationRecords: Long
+): PublicScraper =
+    PublicScraper(
+        id = this.id,
+        name = this.name,
+        totalSubmittedAirQualityRecords = totalSubmittedAirQualityRecords,
+        totalSubmittedRadiationRecords = totalSubmittedRadiationRecords
+    )
 
 fun Application.configureScraperPublicAPI() {
     val scraperService by inject<ScraperService>()
+    val airQualityService by inject<AirQualityService>()
+    val radiationService by inject<RadiationService>()
 
     routing {
         route("/api") {
             route("/scrapers") {
                 // Get all scrapers
                 get {
-                    val scrapers = scraperService.readAll().map { it.toPublicScraper() }
+                    val scrapers = scraperService.readAll().map {
+                        val totalSubmittedAirQualityRecords = airQualityService.getTotalSubmittedRecordsByProvider(it.id)
+                        val totalSubmittedRadiationRecords = radiationService.getTotalSubmittedRecordsByProvider(it.id)
+
+                        it.toPublicScraper(totalSubmittedAirQualityRecords, totalSubmittedRadiationRecords)
+                    }
                     call.respond(HttpStatusCode.OK, scrapers)
                 }
 
                 // Read scraper by ID
                 get("/{id}") {
                     val id = call.parameters["id"]?.toInt() ?: throw IllegalArgumentException("Invalid ID")
-                    val scraper = scraperService.read(id)?.toPublicScraper()
+                    val scraper = scraperService.read(id)
                     if (scraper != null) {
-                        call.respond(HttpStatusCode.OK, scraper)
+
+                        val totalSubmittedAirQualityRecords = airQualityService.getTotalSubmittedRecordsByProvider(id)
+                        val totalSubmittedRadiationRecords = radiationService.getTotalSubmittedRecordsByProvider(id)
+
+                        val publicScraper = scraper.toPublicScraper(
+                            totalSubmittedAirQualityRecords, totalSubmittedRadiationRecords
+                        )
+
+                        call.respond(HttpStatusCode.OK, publicScraper)
                     } else {
                         call.respond(HttpStatusCode.NotFound)
                     }
